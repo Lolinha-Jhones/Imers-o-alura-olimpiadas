1487ca5e6cd6221e846cba5279956b2ff9301a4d
Lolinha-Jhones <lolinhacdv@gmail.com>
2024-09-07 12:22:56 -0300

    Add files via upload

---
+++ filmes.js
@@ -0,0 +1,33 @@
+let filmes = [
+
+        {
+            titulo: "O Iluminado",
+            sinopse: "Um escritor, sua esposa e seu filho pequeno se mudam para um isolado hotel nas montanhas para que ele possa terminar seu novo romance. Mas forças sinistras do hotel começam a assombrar o homem, levando-o à loucura.",
+            ano: 1980,
+            genero: "Terror"
+        },
+        {
+            titulo: "De Volta para o Futuro",
+            sinopse: "Marty McFly, um adolescente, viaja acidentalmente no tempo para 1955 em um DeLorean modificado por seu excêntrico amigo cientista, Doc Brown.",
+            ano: 1985,
+            genero: "Ficção Científica"
+        },
+        {
+            titulo: "Os Goonies",
+            sinopse: "Um grupo de amigos descobre um antigo mapa do tesouro pirata e embarca em uma aventura épica para encontrá-lo.",
+            ano: 1985,
+            genero: "Aventura"
+        },
+        {
+            titulo: "Jurassic Park",
+            sinopse: "Um bilionário realiza o sonho de criar um parque temático com dinossauros clonados, mas a segurança é colocada em risco quando os animais escapam.",
+            ano: 1993,
+            genero: "Ficção Científica"
+        },
+        {
+            titulo: "Pulp Fiction",
+            sinopse: "Uma série de histórias interligadas que se desenrolam em um dia na cidade de Los Angeles, envolvendo criminosos, gangsters e situações absurdas.",
+            ano: 1994,
+            genero: "Crime"
+        }
+    ];
